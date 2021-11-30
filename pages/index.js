07af97fb0c890677db7ba1fff2e90acf95757b5a
prpr48 <prpr48@naver.com>
2021-12-01 00:12:54 +0900

    use per page layout

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
 export default function Home() {
   return <div className="bg-red-400">Hello World</div>;
 }
+
+Home.getLayout = function getLayout(page) {
+  return <div className="p-4">{page}</div>;
+};
