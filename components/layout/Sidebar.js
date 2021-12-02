1fb838ea1f3c59d643f0a10464ee3e9cc948ca22
prpr48 <prpr48@naver.com>
2021-12-02 23:44:52 +0900

    home

---
+++ components/layout/Sidebar.js
@@ -0,0 +1,63 @@
+import {
+  CalendarIcon,
+  ChartBarIcon,
+  FolderIcon,
+  HomeIcon,
+  InboxIcon,
+  UsersIcon,
+} from '@heroicons/react/outline';
+
+const navigation = [
+  { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
+  { name: 'Team', href: '#', icon: UsersIcon, current: false },
+  { name: 'Projects', href: '#', icon: FolderIcon, current: false },
+  { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
+  { name: 'Documents', href: '#', icon: InboxIcon, current: false },
+  { name: 'Reports', href: '#', icon: ChartBarIcon, current: false },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
+
+export default function Sidebar() {
+  return (
+    <div className="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
+      {/* Sidebar component, swap this element with another sidebar if you like */}
+      <div className="flex-1 flex flex-col min-h-0 bg-gray-800">
+        <div className="flex items-center h-16 flex-shrink-0 px-4 bg-gray-900">
+          <img
+            className="h-8 w-auto"
+            src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+            alt="Workflow"
+          />
+        </div>
+        <div className="flex-1 flex flex-col overflow-y-auto">
+          <nav className="flex-1 px-2 py-4 space-y-1">
+            {navigation.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className={classNames(
+                  item.current
+                    ? 'bg-gray-900 text-white'
+                    : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                  'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
+                )}
+              >
+                <item.icon
+                  className={classNames(
+                    item.current ? 'text-gray-300' : 'text-gray-400 group-hover:text-gray-300',
+                    'mr-3 flex-shrink-0 h-6 w-6'
+                  )}
+                  aria-hidden="true"
+                />
+                {item.name}
+              </a>
+            ))}
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,131 +1,29 @@
-import { storefront } from '../utils';
+import { useState } from 'react';
+import Sidebar from '../components/layout/sidebar';
+import SidebarMobile from '../components/layout/SidebarMobile';
+import MainHeader from '../components/layout/MainHeader';
+import MainBody from '../components/layout/MainBody';
 
-const staticProducts = [
-  {
-    id: 1,
-    name: 'Earthen Bottle',
-    href: '#',
-    price: '$48',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
-    imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
-  },
-  {
-    id: 2,
-    name: 'Nomad Tumbler',
-    href: '#',
-    price: '$35',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
-    imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
-  },
-  {
-    id: 3,
-    name: 'Focus Paper Refill',
-    href: '#',
-    price: '$89',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
-    imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
-  },
-  {
-    id: 4,
-    name: 'Machined Mechanical Pencil',
-    href: '#',
-    price: '$35',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
-    imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
-  },
-  // More products...
-];
-
-export default function Home({ products }) {
-  return (
-    <main>
-      <div class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-        <div class="sm:text-center lg:text-left">
-          <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-            Beautiful digital design assets
-          </h1>
-          <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat
-            commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
-          </p>
-          <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-            <div class="rounded-md shadow">
-              <a
-                href="#"
-                class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-900 divide-x divide-gray-600 hover:bg-gray-700 md:py-4 md:text-lg md:px-10"
-              >
-                <span className="pr-6">Get the bundle</span>
-                <span className="pl-6">$199</span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h2 className="sr-only">Products</h2>
-
-        <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {staticProducts.map((item) => {
-            const product = item;
-            return (
-              <a key={product.id} href={product.href} className="group">
-                <div className="w-full aspect-w-4 aspect-h-3 bg-gray-200 rounded-lg overflow-hidden ">
-                  <img
-                    src={product.imageSrc}
-                    alt={product.imageAlt}
-                    className="w-full h-full object-center object-cover group-hover:opacity-75"
-                  />
-                </div>
-                <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-                <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
-              </a>
-            );
-          })}
-        </div>
-      </div>
-    </main>
-  );
+export default function Home() {
+  return <div>Home</div>;
 }
 
 Home.getLayout = function getLayout(page) {
-  return <div>{page}</div>;
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  return (
+    <div>
+      <Sidebar />
+      <SidebarMobile sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+      <div className="md:pl-64 flex flex-col">
+        <MainHeader setSidebarOpen={setSidebarOpen} />
+        <MainBody>{page}</MainBody>
+      </div>
+    </div>
+  );
 };
 
 export async function getStaticProps() {
-  const { data } = await storefront(productsQuery);
   return {
-    props: {
-      // products: data.products,
-    },
+    props: {},
   };
 }
-
-const gql = String.raw;
-
-const productsQuery = gql`
-  query Products {
-    products(first: 6) {
-      edges {
-        node {
-          title
-          handle
-          tags
-          priceRange {
-            minVariantPrice {
-              amount
-            }
-          }
-          images(first: 1) {
-            edges {
-              node {
-                transformedSrc
-                altText
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`;
